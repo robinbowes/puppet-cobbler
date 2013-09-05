fc0f77a8cabb5d44050350565bba0480acc05221
Robin Bowes <robin.bowes@yo61.com>
2013-09-05 08:33:25 +0100

    Don't use sudo in the run script - either run as root or use sudo ./run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
  
 # Assume this script runs from the root of the puppet dir 
 puppetdir="$(pwd)" 
-sudo puppet apply \
+puppet apply \
   --modulepath "$puppetdir"/modules \
   --hiera_config "$puppetdir"/hiera.yaml \
   "$puppetdir"/manifests/site.pp
